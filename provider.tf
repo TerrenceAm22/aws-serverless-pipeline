de5ebd82cad27b59bee1d8725ee5fa047dc8671e
Terrence Malone <tamalone87@gmail.com>
2025-03-10 18:14:18 -0400

    Adding tag

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
+# hinoki and Lotus are the two types of trees that are used in the architecture.
+# The  terraform  block is used to configure the backend. The  backend  block specifies the S3 bucket where the state file will be stored. The  dynamodb_table  attribute is used for state locking.
+# The  provider "aws"  block is used to configure the AWS provider. The  region  attribute specifies the region where the resources will be created.
 terraform {
   backend "s3" {
     bucket         = "your-terraform-states-bucket"
