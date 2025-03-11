f07b38f290690b09fe6d79de69668ef56a3030a4
Terrence Malone <tamalone87@gmail.com>
2025-03-11 10:59:53 -0400

    creating sqs queue

--- sqs.tf
+++ sqs.tf
@@ -1,3 +1,9 @@
+# Create an SQS Queue for Analytics Processing
+resource "aws_sqs_queue" "analytics_queue" {
+  name = "AnalyticsProcessingQueue"
+}
+
+
 resource "aws_sqs_queue" "submission_queue" {
   name                       = "DataSubmissionQueue"
   delay_seconds              = 0
