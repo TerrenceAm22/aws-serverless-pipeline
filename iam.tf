af128123717fd30bc404067594238daacf7014c0
Terrence Malone <tamalone87@gmail.com>
2025-03-11 11:24:31 -0400

    delete duplicate

--- iam.tf
+++ iam.tf
@@ -84,21 +84,6 @@ resource "aws_iam_role_policy_attachment" "lambda_eventbridge_attach" {
 }
 
 
-resource "aws_iam_policy" "lambda_sqs_policy" {
-  name        = "LambdaSQSPolicy"
-  description = "IAM policy to allow Lambda to send messages to SQS"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = "sqs:SendMessage",
-        Resource = aws_sqs_queue.submission_queue.arn
-      }
-    ]
-  })
-}
-
 resource "aws_iam_policy" "terraform_s3_policy" {
   name        = "TerraformS3StatePolicy"
   description = "IAM policy to allow Terraform to read and write state to S3"
