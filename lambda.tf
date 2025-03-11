5676868d963687dd1af5fbd8a10e0956b4616824
Terrence Malone <tamalone87@gmail.com>
2025-03-11 17:14:01 -0400

    updating tf lambda

--- lambda.tf
+++ lambda.tf
@@ -30,11 +30,11 @@ resource "aws_lambda_function" "analytics_processor" {
   source_code_hash = filebase64sha256("analytics_function.zip")
 
   environment {
-    variables = {
-      ANALYTICS_BUCKET = aws_s3_bucket.analytics_data.id
-      ANALYTICS_TABLE  = aws_dynamodb_table.analytics_table.name
-    }
+  variables = {
+    ANALYTICS_BUCKET = "analytics-data-bucket-571600861898"
+    ANALYTICS_TABLE  = "AnalyticsDynamoDBTable"
   }
+}
 }
 
 
