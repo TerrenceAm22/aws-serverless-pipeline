0bc66d319ce992700fdcf9c910419944029f4b90
Terrence Malone <tamalone87@gmail.com>
2025-03-14 14:18:34 -0400

    config new eventbridge

--- eventbridge.tf
+++ eventbridge.tf
@@ -18,6 +18,42 @@ resource "aws_cloudwatch_event_rule" "new_data_submission_rule" {
   })
 }
 
+
+resource "aws_sns_topic" "lambda_event_topic" {
+  name = "lambda-execution-events"
+}
+
+resource "aws_cloudwatch_event_target" "sns_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_execution_rule.name
+  arn       = aws_sns_topic.lambda_event_topic.arn
+
+  # Ensures the SNS Topic is created before the Event Target
+  depends_on = [aws_cloudwatch_event_rule.new_data_submission_rule]
+}
+
+resource "aws_iam_policy" "eventbridge_sns_policy" {
+  name        = "EventBridgeSNSPublishPolicy"
+  description = "Allows EventBridge to publish messages to SNS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "sns:Publish",
+        Resource = aws_sns_topic.lambda_event_topic.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eventbridge_sns_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.eventbridge_sns_policy.arn
+}
+
+
+
 # Ensure Rule Exists Before Creating Target
 resource "aws_cloudwatch_event_target" "analytics_lambda_target" {
   rule           = aws_cloudwatch_event_rule.new_data_submission_rule.name
