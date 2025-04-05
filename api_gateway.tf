7e586e35b9f5a9c4572f40489f8ae99f8b1b16e9
Terrence Malone <tamalone87@gmail.com>
2025-04-04 20:33:23 -0400

    Demonstrating

--- api_gateway.tf
+++ api_gateway.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "submit_data" {
   parent_id   = aws_api_gateway_rest_api.data_api.root_resource_id
   path_part   = "submitData"
 }
-
+# Demonstrating
 resource "aws_api_gateway_resource" "get_data" {
   rest_api_id = aws_api_gateway_rest_api.data_api.id
   parent_id   = aws_api_gateway_rest_api.data_api.root_resource_id
